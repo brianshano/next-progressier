0144a2b890cf7703cd7256fd3360333061adb2e9
Brian Shanley <bshanley@gmail.com>
2024-07-23 22:33:22 +0100

    progressier pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="manifest"
+        href="https://progressier.app/IUVNwJ9e3vmpXI5nPhhF/progressier.json"
+      />
+
       <body className={inter.className}>{children}</body>
+      <Script
+        src="https://progressier.app/IUVNwJ9e3vmpXI5nPhhF/script.js"
+        strategy="beforeInteractive"
+      />
     </html>
   );
 }
